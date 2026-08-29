e69b7275e96d079a7d03ee02fe5fc660eb48eba7
khushishigwan07-jpg <khushishigwan07@gmail.com>
2026-08-29 06:28:11 +0530

    Fix GitHub Pages asset path

--- maharashtra-monitor/vite.config.js
+++ maharashtra-monitor/vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SIH-2026_MPLADS/',
 })
